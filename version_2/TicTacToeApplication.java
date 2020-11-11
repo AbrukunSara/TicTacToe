19018c81369a53fee97403d39abc13b59769425a
AbrukunSara <58888164+AbrukunSara@users.noreply.github.com>
2020-11-12 01:16:11 +0600

    Author name changed in TicTacToeApplication file

--- version_2/TicTacToeApplication.java
+++ version_2/TicTacToeApplication.java
@@ -2,7 +2,7 @@
  * Contains the main method. Creates a ConsoleRunner and then calls its
  * mainLoop method.
  *
- * @author INSERT YOUR NAME(S) HERE
+ * @author Sara Abrukun Ahmmed
  */
 
 public class TicTacToeApplication {
